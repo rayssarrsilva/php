bbc2e097ead5db86c785aa11054871250316707c
rayssa <116031703+rayssarrsilva@users.noreply.github.com>
2024-10-17 16:23:57 -0300

    Update login.php
    adição de segurança no login contra inject sql

--- login.php
+++ login.php
@@ -1,22 +1,31 @@
 <?php
-
 include("conexao.php");
 
 $cpf=$_POST["cpf"];
 $senha=$_POST["senha"];
 
-$sql = "select nome from usuarios where cpf='$cpf' and senha='$senha'";
+$sql = "select nome from usuarios where cpf=? and senha=?";
+$stmt = $conn->prepare($sql);
 
-$resultado = $conn->query($sql);
-$row = $resultado->fetch_assoc();
+if ($stmt) {
+    $stmt->bind_param("ss", $cpf, $senha);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    
+    $resultado = $conn->query($sql);
+    $row = $resultado->fetch_assoc();
 
-if(isset($row) && $row['nome'] != ''){
-    session_start();
-    $_SESSION["cpf"] = $cpf;
-    $_SESSION["senha"] = $senha;
-    $_SESSION["nome"] = $row['nome'];
-    header("Location: principal.php");
+if($result->num_rows > 0){
+    $row = $result->fetch_assoc();
+    if($row['nome'] != ''){
+        session_start();
+        $_SESSION["cpf"] = $cpf;
+        $_SESSION["senha"] = $senha;
+        $_SESSION["nome"] = $row['nome'];
+        header("Location: principal.php"); 
+    }
 }else{
     echo "senha incorreta";
+    }
 }
 ?>
