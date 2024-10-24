694ba90550e12a067a48ee78ef69ec2a9a44b846
rayssa <116031703+rayssarrsilva@users.noreply.github.com>
2024-10-24 19:55:46 -0300

    Update login.php

--- login.php
+++ login.php
@@ -1,31 +1,22 @@
 <?php
+
 include("conexao.php");
 
 $cpf=$_POST["cpf"];
 $senha=$_POST["senha"];
 
-$sql = "select nome from usuarios where cpf=? and senha=?";
-$stmt = $conn->prepare($sql);
+$sql = "select nome from usuarios where cpf='$cpf' and senha='$senha'";
 
-if ($stmt) {
-    $stmt->bind_param("ss", $cpf, $senha);
-    $stmt->execute();
-    $result = $stmt->get_result();
-    
-    $resultado = $conn->query($sql);
-    $row = $resultado->fetch_assoc();
+$resultado = $conn->query($sql);
+$row = $resultado->fetch_assoc();
 
-if($result->num_rows > 0){
-    $row = $result->fetch_assoc();
-    if($row['nome'] != ''){
-        session_start();
-        $_SESSION["cpf"] = $cpf;
-        $_SESSION["senha"] = $senha;
-        $_SESSION["nome"] = $row['nome'];
-        header("Location: principal.php"); 
-    }
+if(isset($row) && $row['nome'] != ''){
+    session_start();
+    $_SESSION["cpf"] = $cpf;
+    $_SESSION["senha"] = $senha;
+    $_SESSION["nome"] = $row['nome'];
+    header("Location: principal.php");
 }else{
     echo "senha incorreta";
-    }
 }
 ?>
