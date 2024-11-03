da266cbd083467c50c5834a34348eb215b2d209a
rayssa <116031703+rayssarrsilva@users.noreply.github.com>
2024-11-02 21:02:53 -0300

    Update login.php
    regex do cpf para no minimo 11 caracteres

--- login.php
+++ login.php
@@ -4,6 +4,11 @@ include("conexao.php");
 $cpf = $_POST["cpf"];
 $senha = $_POST["senha"];
 
+if (!preg_match('/^\d{11}$/', $cpf)) {
+    echo "O CPF deve conter exatamente 11 dígitos.";
+    exit;
+} // verifica se tem no minimo 11 digitos
+
 // Verificação da senha usando regex (pode ser feito em JavaScript antes do envio)
 if (!preg_match('/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{6,}$/', $senha)) {
     echo "A senha deve ter pelo menos 6 caracteres, incluindo uma letra maiúscula, uma letra minúscula, um número e um caractere especial.";
