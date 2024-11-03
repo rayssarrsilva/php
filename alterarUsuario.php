e8135fff18d82094b07736c0475d5945d28c7756
rayssa <116031703+rayssarrsilva@users.noreply.github.com>
2024-11-02 21:02:14 -0300

    Update alterarUsuario.php
    espressores gulares pra limitar cpf e senha

--- alterarUsuario.php
+++ alterarUsuario.php
@@ -6,6 +6,17 @@ $nome = $_POST["nome"];
 $senha = $_POST["senha"];
 $cpfAnterior = $_POST["cpfAnterior"];
 
+if (!preg_match('/^\d{11}$/', $cpf)) {
+    echo "O CPF deve conter exatamente 11 dígitos.";
+    exit;
+} // verifica se tem no minimo 11 digitos
+
+// Verificação da senha usando regex (pode ser feito em JavaScript antes do envio)
+if (!preg_match('/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{6,}$/', $senha)) {
+    echo "A senha deve ter pelo menos 6 caracteres, incluindo uma letra maiúscula, uma letra minúscula, um número e um caractere especial.";
+    exit;
+}
+
 $sql = "update usuarios set cpf = '$cpf',
                             senha = '$senha',
                             nome = '$nome'
